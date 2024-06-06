fce462d30cd6658900f6aeded902d510216c5d21
renawmontanari <regiangelica@gmail.com>
2024-06-06 20:07:36 -0300

    criando-spa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Inicio from "./componentes/Paginas/Inicio";
-import SobreMim from "./componentes/Paginas/SobreMim";
+import Inicio from "./Paginas/Inicio";
+import SobreMim from "./Paginas/SobreMim";
+import Menu from "./componentes/Menu";
 
 function App() {
   return (
     <BrowserRouter>
+      <Menu />
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/sobremim" element={<SobreMim />} />
