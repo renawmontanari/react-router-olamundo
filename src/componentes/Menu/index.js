83de438f68d7fa8a8372ba3953e6495c26371522
renawmontanari <regiangelica@gmail.com>
2024-06-07 20:11:46 -0300

    use-location

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -1,16 +1,17 @@
-import { Link } from 'react-router-dom';
+
 import styles from './Menu.module.css';
+import MenuLink from '../MenuLink';
 
 export default function Menu() {
     return (
         <header>
             <nav className={styles.navegacao}>
-                <Link className={styles.link} to='/'>
+                <MenuLink to="/">
                     Inicio
-                </Link>
-                <Link className={styles.link} to='/sobremim'>
-                    Sobre mim
-                </Link>
+                </MenuLink>
+                <MenuLink to="/sobremim">
+                    Sobre Mim
+                </MenuLink>
             </nav>
         </header>
     )
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import Inicio from "./Paginas/Inicio";
 import SobreMim from "./Paginas/SobreMim";
 import Menu from "./componentes/Menu";
 
-function App() {
+function AppRoutes() {
   return (
     <BrowserRouter>
       <Menu />
@@ -16,4 +16,4 @@ function App() {
   );
 }
 
-export default App;
+export default AppRoutes;
